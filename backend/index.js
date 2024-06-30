235452111b567359283ce3f125848e2af58b57b4
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-30 16:29:54 +0530

    JWT sectret ,Add jsonwebtoken, add body parser

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var cors = require("cors");
 
 const rootRouter = require("./routes/index");
 
 const app = express();
-
+app.use(express.json());
+app.use(cors());
 app.use("/api/v1", rootRouter);
+app.listen(3000);
