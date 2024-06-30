c9b3a5c47b8944404062ff68f23d31fe540fb65d
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-30 21:25:22 +0530

    added acounts schema in db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -13,9 +13,22 @@ const userSchema = new Schema({
   firstName: String,
   lastName: String,
 });
+const accountSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to User model
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports({
   User,
+  Account,
 });
