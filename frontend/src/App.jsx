6f20350a0c731f4615f4cfb01b2ee733e934a304
SHORYA BAJ <jain.shorya11@gmail.com>
2024-07-02 21:05:28 +0530

    pages and tailwind , react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Router } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Send from "./pages/Send";
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Router>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/send" element={<Send />} />
+          </Router>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
